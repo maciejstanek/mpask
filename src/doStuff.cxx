9ab788126953c12d25c5e5c03a027594dc9f2617
Maciej Stanek <makaron28@gmail.com>
2018-10-20 18:20:35 +0200

    Added boost.

--- src/doStuff.cxx
+++ src/doStuff.cxx
@@ -1,14 +1,16 @@
 #include "mibparser/doStuff.hxx"
 
+#include <boost/lexical_cast.hpp>
 #include <iostream>
 
 using namespace std;
+using namespace boost;
 
 namespace mibparser {
 
 int doStuff(int thing)
 {
-  cerr << "Called 'mibparser::doStuff(" << thing << ")'." << endl;
+  cerr << "Called 'mibparser::doStuff(" << lexical_cast<std::string>(thing) << ")'." << endl;
   return (thing + 2) % 4;
 }
 
