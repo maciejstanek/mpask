a1afea4a485f4ead97e7074ae03d252249b1b21d
Maciej Stanek <makaron28@gmail.com>
2018-11-25 21:28:59 +0100

    Fixed testing script - now properly removing old test results.

--- meta/dot_unflatten.sh
+++ meta/dot_unflatten.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running 'dot_unflatten.sh' in '$PWD'."
-rm -vf ../tests/*.flat.dot
+rm -f $(find ../tests -name '*.flat.dot')
 for dot in $(find ../tests -name '*.dot'); do
   echo "Calling 'unflatten' on '$dot'."
   unflatten -l 6 -f $dot > ${dot%.*}.flat.dot
