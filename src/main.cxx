f2d458994ae372663b78b9e9651b325159bdecbf
Maciej Stanek <makaron28@gmail.com>
2018-11-26 21:38:07 +0100

    Added this to the main.

--- src/main.cxx
+++ src/main.cxx
@@ -12,13 +12,15 @@ namespace po = boost::program_options;
 int
 main(int argc, char *argv[])
 {
-  cerr << "[DBG] Starting 'mpask_exe'" << endl;
+  cerr << "[MPASK] Starting 'mpask_exe'" << endl;
 
   po::options_description description("Allowed options");
   description.add_options()
     ("help,h", "print this help message")
     ("directory,d", po::value<string>(), "working directory, defaults to current directory")
     ("file,f", po::value<string>(), "input file name")
+    ("name,n", po::value<string>(), "do search by name")
+    ("oid,o", po::value<string>(), "display specific oid")
   ;
   po::variables_map variables;
   po::store(po::parse_command_line(argc, argv, description), variables);
@@ -43,7 +45,19 @@ main(int argc, char *argv[])
   if (variables.count("directory")) {
     workingDirectory = variables["directory"].as<string>();
   }
-  cerr << "[DBG] Working directory: '" << workingDirectory << "'" << endl;
+  cerr << "[MPASK] Working directory: '" << workingDirectory << "'" << endl;
+
+	string searchedName;
+  if (variables.count("name")) {
+    searchedName = variables["name"].as<string>();
+  }
+  cerr << "[MPASK] Searched name: '" << searchedName << "'" << endl;
+
+	string oidToDisplay;
+  if (variables.count("oid")) {
+    oidToDisplay = variables["oid"].as<string>();
+  }
+  cerr << "[MPASK] OID to display: '" << oidToDisplay << "'" << endl;
 
 	string fileName;
   if (variables.count("file")) {
@@ -53,22 +67,49 @@ main(int argc, char *argv[])
     cerr << "Try 'mpask_exe --help' for more information." << endl;
     return 1;
   }
-  cerr << "[DBG] File name: '" << fileName << "'" << endl;
+  cerr << "[MPASK] File name: '" << fileName << "'" << endl;
 
-  cerr << "[DBG] Calling master parser" << endl;
+  cerr << "[MPASK] Calling master parser" << endl;
   auto object = MasterParser{}(workingDirectory, fileName);
-  cerr << "[DBG] Calling tree builder" << endl;
+  cerr << "[MPASK] Calling tree builder" << endl;
   auto root = TreeBuilder{}(object);
-  cerr << "[DBG] Printing tree hierarchy" << endl;
+  cerr << "[MPASK] Printing tree hierarchy" << endl;
   root->printHierarchy(cout, "-  ");
 
   string dotFile {workingDirectory + "/"s + fileName + ".dot"s};
-  cerr << "[DBG] Printing dot file to '" << dotFile << "'" << endl;
+  cerr << "[MPASK] Printing dot file to '" << dotFile << "'" << endl;
   ofstream dot;
   dot.open(dotFile);
   root->printDotFile(dot);
   dot.close();
+  
+  if (!searchedName.empty()) {
+    cerr << "[MPASK] Searching the tree for name '" << searchedName << "'" << endl;
+    try {
+      auto result = root->findOidByName(searchedName);
+      auto textOid = root->generateOID(result);
+      cerr << "[MPASK] Found OID '" << textOid << "'" << endl;
+      if (oidToDisplay.empty()) {
+        cerr << "[MPASK] Assuming display request for '" << textOid << "'" << endl;
+        oidToDisplay = textOid;
+      }
+    } 
+    catch (const std::exception& e) {
+      cerr << "[MPASK] Searching failed with the following message: '" << e.what() << "'" << endl;
+    }
+  }
 
-  cerr << "[DBG] Closing executable" << endl;
+  if (!oidToDisplay.empty()) {
+    cerr << "[MPASK] Trying to display the node '" << oidToDisplay << "'" << endl;
+    try {
+      auto result = root->findNodeByOID(oidToDisplay);
+      cerr << result->getSource() << endl;
+    } 
+    catch (const std::exception& e) {
+      cerr << "[MPASK] Searching failed with the following message: '" << e.what() << "'" << endl;
+    }
+  }
+
+  cerr << "[MPASK] Closing executable" << endl;
   return 0;
 }
